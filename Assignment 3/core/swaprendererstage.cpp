45414cc8eccfc7a40f40c89bc4aa6d3b215b46a3
Alex <alexstephens9@gmail.com>
2019-05-26 16:52:36 +1000

    fix bug in test mode keypress event

--- Assignment 3/core/swaprendererstage.cpp
+++ Assignment 3/core/swaprendererstage.cpp
@@ -6,7 +6,7 @@ SwapRendererStage::SwapRendererStage(std::unique_ptr<GameStage> stage) : GameSta
 }
 
 void SwapRendererStage::input(QKeyEvent &event) {
-    if (event.key() == Qt::Key_1) {
+    if (event.type() == QEvent::KeyPress && event.key() == Qt::Key_1) {
         debug = !debug;
     }
     GameStageDecorator::input(event);
